bff0bb2abb496378bb0b6d4769eaccf3861dfe28
Malcolm <crummynz@gmail.com>
2015-06-12 13:19:09 +0200

    Basic and complex bulidings drawn in an array

--- city.js
+++ city.js
@@ -33,22 +33,64 @@ function init() {
   scene.add(plane);
   
   var geometry = new THREE.BoxGeometry(1, 1, 1);
-  geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
+  var center = new THREE.Matrix4().makeTranslation( 0, 0.5, 0 );
+  var tall = new THREE.Matrix4().makeScale(1, 2, 1);
+  geometry.applyMatrix(center);
+  geometry.applyMatrix(tall);
   
-  var building = new THREE.Mesh(geometry);
+  var buildings = new Buildings();
   var city = new THREE.Geometry();
-  
-  var color = new THREE.Color().setRGB(0.5, 0.2, 0.8);
+  for (var x = 0; x < cityBlocksX; ++x) {
+    for (var z = 0; z < cityBlocksZ; ++z) {
+      var building = buildings.random(x, z);
+      city.merge(building, building.matrix);
+    }
+  }
 
-  var geometry = building.geometry;
-  building.updateMatrix();
-  city.merge(building.geometry, building.matrix);
-  
   var mesh = new THREE.Mesh(city, new THREE.MeshLambertMaterial({vertexColors: THREE.VertexColors}));
   
   scene.add(mesh);
   
   lastTime = performance.now();
+}
+
+function Buildings() {
+  var blockSize = 0.8;
+  var box = new THREE.BoxGeometry(1, 1, 1);
+  var initialMatrix = new THREE.Matrix4().makeTranslation(0, 0.5, 0);
+  
+  this.random = function(x, z) {
+    return Math.random() > 0.5 ? this.bland(x,z) : this.modern(x,z); 
+  }
+  
+  this.bland = function(x, z, width, height) {
+    if (width == undefined) width = blockSize;
+    if (height == undefined) height = blockSize;
+    
+    // start from a simple box
+    var geometry = box.clone();
+    // shift center, so transforms occur from bottom of box
+    geometry.applyMatrix(initialMatrix);
+    // grow it upwards, and squeeze it to allow for streets in between
+    var heightAddition = Math.random() * Math.random() * 2;
+    var scaleMatrix = new THREE.Matrix4().makeScale(1 * width, 1 + heightAddition, 1 * height);
+    geometry.applyMatrix(scaleMatrix);
+    // shift it into place
+    var translationMatrix = new THREE.Matrix4().makeTranslation(x, 0, z);
+    geometry.applyMatrix(translationMatrix);
+    return geometry;
+  }
+  
+  this.modern = function(x, z) {
+    var geometry = new THREE.Geometry();
+    var box1 = this.bland(x, z, Math.random(), Math.random());
+    var box2 = this.bland(x, z, Math.random(), Math.random());
+    var box3 = this.bland(x, z, Math.random(), Math.random());
+    geometry.merge(box1, box1.matrix);
+    geometry.merge(box2, box2.matrix);
+    geometry.merge(box3, box3.matrix);
+    return geometry;
+  }
 }
 
 function animate() {
