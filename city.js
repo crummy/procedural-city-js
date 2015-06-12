51ad3c0d282ef1899182173537e345c5c8379f6f
Malcolm <crummynz@gmail.com>
2015-06-12 13:27:04 +0200

    Textured buildings

--- city.js
+++ city.js
@@ -46,8 +46,12 @@ function init() {
       city.merge(building, building.matrix);
     }
   }
+  
+  var texture = new THREE.Texture(generateTexture());
+  texture.anisotropy = renderer.getMaxAnisotropy();
+  texture.needsUpdate = true;
 
-  var mesh = new THREE.Mesh(city, new THREE.MeshLambertMaterial({vertexColors: THREE.VertexColors}));
+  var mesh = new THREE.Mesh(city, new THREE.MeshLambertMaterial({map: texture, vertexColors: THREE.VertexColors}));
   
   scene.add(mesh);
   
@@ -99,4 +103,32 @@ function animate() {
   controls.update( time - lastTime );
   renderer.render( scene, camera ); 
   lastTime = time;
+}
+
+function generateTexture() {
+  var canvas = document.createElement( 'canvas' );
+  canvas.width = 32;
+  canvas.height = 64;
+
+  var context = canvas.getContext( '2d' );
+  context.fillStyle = '#ffffff';
+  context.fillRect( 0, 0, 32, 64 );
+
+  for ( var y = 2; y < 64; y += 2 ) {
+    for ( var x = 0; x < 32; x += 2 ) {
+      var value = Math.floor( Math.random() * 64 );
+      context.fillStyle = 'rgb(' + [ value, value, value ].join( ',' )  + ')';
+      context.fillRect( x, y, 2, 1 );
+    }
+  }
+
+  var canvas2 = document.createElement( 'canvas' );
+  canvas2.width = 512;
+  canvas2.height = 1024;
+
+  var context = canvas2.getContext( '2d' );
+  context.imageSmoothingEnabled = false;
+  context.drawImage( canvas, 0, 0, canvas2.width, canvas2.height );
+
+  return canvas2;
 }
